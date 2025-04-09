59d16de28a375eabd07262585b19260d8898d0e3
Edison Matos <edison.matos@live.com>
2025-04-08 22:49:40 -0300

    Initial commit

---
+++ src/components/cards/HowItWorksCard.jsx
@@ -0,0 +1,38 @@
+import MotionDivDownToUp from "../animation/MotionDivDownToUp";
+
+export default function HowItWorksCard({
+  number,
+  title,
+  description,
+  animation = true,
+}) {
+  const Content = (
+    <div className="flex flex-col items-center justify-center h-auto transition desktop1:items-center font-mainFont desktop1:hover:scale-110">
+      <div className="bg-bgSectionDark mb-[32px] p-[25px] flex justify-center items-center rounded-full h-[48px] w-[48px] text-title1 text-white font-semibold">
+        {number}
+      </div>
+      <h1 className="text-[20px] text-black font-bold mb-[24px]">{title}</h1>
+      <p className="text-[16px] text-black opacity-70 text-center desktop1:text-center w-full">
+        {description}
+      </p>
+    </div>
+  );
+
+  const ContentStatic = (
+    <div className="flex flex-col items-center justify-center tablet1:w-[45%] transition desktop1:items-center font-mainFont desktop1:hover:scale-110 h-auto">
+      <div className="bg-bgSectionDark mb-[32px] p-[25px] flex justify-center items-center rounded-full h-[48px] w-[48px] text-black text-title1 font-semibold">
+        {number}
+      </div>
+      <h1 className="text-[20px] text-black font-bold mb-[24px]">{title}</h1>
+      <p className="text-[16px] text-black opacity-70 text-center desktop1:text-center w-full">
+        {description}
+      </p>
+    </div>
+  );
+
+  return animation ? (
+    <MotionDivDownToUp className="tablet1:w-[45%]">{Content}</MotionDivDownToUp>
+  ) : (
+    ContentStatic
+  );
+}
